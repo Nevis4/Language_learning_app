0bf43912934233755bfc78aa3d8046664e5c120b
Nevis4 <przemyslaw.malara@icloud.com>
2022-02-02 00:22:04 +0100

    Creating a widget using a class that inherits from QtWidgets

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+import sys
+
 from PySide2 import QtCore, QtWidgets, QtGui
 
+
+class AppWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.label = QtWidgets.QLabel('Hello World')
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.label)
+        self.setLayout(self.layout)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    appWidget = AppWidget()
+    appWidget.resize(800, 600)
+    appWidget.show()
+    sys.exit(app.exec_())
