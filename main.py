09eebd4d2152ab12715066a9f848e00528695394
Nevis4 <przemyslaw.malara@icloud.com>
2022-02-02 13:35:05 +0100

    Adding a new method to the AppWidget class and adding the main function

--- main.py
+++ main.py
@@ -3,18 +3,42 @@ import sys
 from PySide2 import QtCore, QtWidgets, QtGui
 
 
+# class AppWidget inherits from QtWidgets class
 class AppWidget(QtWidgets.QWidget):
-    def __init__(self):
+    # fn init accepts 2 arguments
+    # self and dictionary to translate
+    def __init__(self, dictionary_translate):
         super().__init__()
-        self.label = QtWidgets.QLabel('Hello World')
-        self.layout = QtWidgets.QVBoxLayout()
-        self.layout.addWidget(self.label)
+        self.dictionary = dictionary_translate
+        self.layout = self.initialize()
         self.setLayout(self.layout)
 
+    def initialize(self):
+        label_row = 0
+        area = QtWidgets.QGridLayout()
+        for key in self.dictionary:
+            label = QtWidgets.QLabel(key)
+            enter = QtWidgets.QLineEdit()
+            area.addWidget(label, label_row, 0)
+            area.addWidget(enter, label_row, 1)
+            label_row += 1
 
-if __name__ == '__main__':
+        return area
+
+
+def main():
+    dictionary = {
+        'dog': 'pies',
+        'cat': 'kot',
+        'hello': 'witaj'
+    }
     app = QtWidgets.QApplication([])
-    appWidget = AppWidget()
+    app.setApplicationDisplayName('Language learning by Przemyslaw Malara')
+    appWidget = AppWidget(dictionary)
     appWidget.resize(800, 600)
     appWidget.show()
     sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
