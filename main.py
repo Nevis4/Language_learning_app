9685f2d2c844df995291b4fc77ac84a1406c21f0
Nevis4 <przemyslaw.malara@icloud.com>
2022-02-02 15:29:56 +0100

    Final Version 1.0 -Created class Translation for elemetns in area -Created btn in area and fn btn_click for calculate player points -Change fn initialize

--- main.py
+++ main.py
@@ -3,26 +3,60 @@ import sys
 from PySide2 import QtCore, QtWidgets, QtGui
 
 
+# class for elements in area
+class Translation:
+    def __init__(self, to_translate, input_form, translated, correct_translation):
+        self.to_translate = to_translate
+        self.input_form = input_form
+        self.translated = translated
+        self.correct_translation = correct_translation
+
+
 # class AppWidget inherits from QtWidgets class
 class AppWidget(QtWidgets.QWidget):
     # fn init accepts 2 arguments
     # self and dictionary to translate
     def __init__(self, dictionary_translate):
         super().__init__()
+        self.points = 0
         self.dictionary = dictionary_translate
+        self.state = []
         self.layout = self.initialize()
         self.setLayout(self.layout)
 
+    # btn function
+    def btn_click(self):
+        self.points = 0
+        for inline in self.state:
+            if inline.correct_translation == inline.input_form.text():
+                self.points += 1
+        msg = QtWidgets.QMessageBox()
+        msg.setText(f'You have {self.points} points')
+        msg.exec_()
+
+    # function for make and return area
     def initialize(self):
         label_row = 0
         area = QtWidgets.QGridLayout()
-        for key in self.dictionary:
-            label = QtWidgets.QLabel(key)
+
+        # create word_to_translate and input
+        for key, correct_translation in self.dictionary.items():
+            word_to_translate = QtWidgets.QLabel(key)
             enter = QtWidgets.QLineEdit()
-            area.addWidget(label, label_row, 0)
+            self.state.append(Translation(key, enter, '', correct_translation))
+
+            area.addWidget(word_to_translate, label_row, 0)
             area.addWidget(enter, label_row, 1)
             label_row += 1
 
+        # button
+        btn_check = QtWidgets.QPushButton("Check")
+        btn_check.clicked.connect(self.btn_click)
+        area.addWidget(
+            btn_check,
+            label_row,
+            1
+        )
         return area
 
 
@@ -30,7 +64,7 @@ def main():
     dictionary = {
         'dog': 'pies',
         'cat': 'kot',
-        'hello': 'witaj'
+        'hello': 'witaj',
     }
     app = QtWidgets.QApplication([])
     app.setApplicationDisplayName('Language learning by Przemyslaw Malara')
